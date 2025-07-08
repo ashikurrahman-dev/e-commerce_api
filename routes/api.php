0d09433d3bc4bcd28c86990dd25a20cf737cb444
ashik <ashikurrahman7134@gmail.com>
2025-07-08 16:52:04 +0600

    authentication route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticateController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [AuthenticateController::class, 'login']);
+Route::get('/logout', [AuthenticateController::class, 'logout'])->middleware('auth:sanctum');
