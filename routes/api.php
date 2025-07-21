5f5e1a034b807b6add9c760f9189e9e114937fa9
ashik <ashikurrahman7134@gmail.com>
2025-07-21 08:44:18 +0600

    update route

--- routes/api.php
+++ routes/api.php
@@ -12,19 +12,13 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
-
-
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [AuthenticateController::class, 'login']);
 // admin login
 Route::post('/admin/login', [AuthenticateController::class, 'login']);
-Route::get('/logout', [AuthenticateController::class, 'logout'])->middleware('auth:sanctum');
 
+Route::post('/logout', [AuthenticateController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware([AdminMiddleware::class, 'auth:sanctum'])->get('/admin', function () {
-    return 'Welcome to Admin Panel!';
-});
 
 Route::prefix('/admin')->middleware(['auth:sanctum', AdminMiddleware::class])->group(function () {
     Route::apiResource('/categories', CategoryController::class);
@@ -44,7 +38,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/cart/{id}', [CartController::class, 'update']);
     Route::delete('/cart/{id}', [CartController::class, 'destroy']);
     // order route
-    Route::post('/orders', [OrderItemController::class, 'store']);       // Place order
-    Route::get('/orders', [OrderItemController::class, 'index']);        // List orders
+    Route::post('/orders', [OrderItemController::class, 'store']);
+    Route::get('/orders', [OrderItemController::class, 'index']);
     Route::get('/orders/{id}', [OrderItemController::class, 'show']);
 });
